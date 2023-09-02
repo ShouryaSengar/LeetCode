1355445ee3a787700ce14e39b1ba0479b24e726d
Shourya Singh Sengar <93751651+ShouryaSengar@users.noreply.github.com>
2023-09-02 12:57:24 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (32.30%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*USING STL only*/
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0; i<=n; i++) {
@@ -7,4 +8,19 @@ public:
         }
         return ans;
     }
+    /*USING DP*/
+    // vector<int> countBits(int n) {
+    //     vector<int>dp(n+1,0);
+    //     for(int i = 0 ; i<=n;i++)
+    //     {
+    //         if(i%2==0)
+    //         {
+    //             dp[i]=dp[i/2];
+    //         }
+    //         else
+    //         {
+    //             dp[i]=dp[i/2]+1;
+    //         }
+    //     }
+    //     return dp;
 };
